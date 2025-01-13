59d8fd3b201bb27994daa901e38ec535f09aaa18
ARUN A MENON <52127560+arunamenon@users.noreply.github.com>
2025-01-13 03:58:50 +0000

    updates

---
+++ app/controllers/directors_controller.rb
@@ -0,0 +1,25 @@
+class DirectorsController < ApplicationController
+  def index
+    render({ :template => "director_templates/list"})
+  end
+
+  def show
+    the_id = params.fetch("the_id")
+    matching_records = Director.where({ :id => the_id })
+    @the_director = matching_records.at(0)
+    render({ :template => "director_templates/details" })
+  end
+
+  def junior
+    matching_records = Director.where.not({ :dob => nil }).order({ :dob => :desc })
+    @the_director = matching_records.at(0)
+    render({ :template => "director_templates/youngest" })
+  end
+
+  def eldest
+    matching_records = Director.where.not({ :dob => nil }).order({ :dob => :asc })
+    @the_director = matching_records.at(0)
+    render({ :template => "director_templates/eldest" })
+  end
+  
+end
